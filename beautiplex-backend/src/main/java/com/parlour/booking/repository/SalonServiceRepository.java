00c4b46cbb36f39e85b4783e1e7f75705f7ad3ff
Vishwas Pratap Singh Pal <vishwaspratapsinghpal@gmail.com>
2025-02-22 17:19:54 +0530

    removed files

--- beautiplex-backend/src/main/java/com/parlour/booking/repository/SalonServiceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.parlour.booking.repository;
-
-import com.parlour.booking.model.ParlourService;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SalonServiceRepository extends JpaRepository<ParlourService, Long> {
-}
